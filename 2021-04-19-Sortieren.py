6c09e3e3fc58e8d78fdc3deea58131d86d4eeb93
Santino-lab <80707158+Santino-lab@users.noreply.github.com>
2021-05-04 16:15:26 +0200

    .
    .

---
+++ 2021-04-19-Sortieren.py
@@ -0,0 +1,29 @@
+### Aufgabe 1: Erstelle eine Liste mit beliebigen zehn verschiedenen Zahlen und speichere diese in einer Variable! ###^
+"""liste = [1,2,3,4,5,6,7,8,9,10]
+kleinstezahl=liste[0]
+for zahl in liste :
+    if zahl < kleinstezahl:
+        kleinstezahl = zahl
+print (kleinstezahl)
+
+
+liste = [1,2,3,4,5,6,7,8,9,10]
+gröstezahl=liste[0]
+for zahl in liste :
+    if zahl > gröstezahl:
+        gröstezahl = zahl
+print (gröstezahl)"""
+
+
+liste = [10,2,3,4,5,6,7,8,9,1]
+zweitgröstezahl=liste[9]
+gröstezahl=liste[1]
+for zahl in liste :
+    if zahl > gröstezahl:
+        zweitgröstezahl=gröstezahl
+        gröstezahl = zahl
+    if zahl > zweitgröstezahl:
+        zweitgröstezahl = zahl
+print (zweitgröstezahl)
+print (gröstezahl)
+
