7bcd07dde7f467975c0f4bd2695f6ab3f3defbf6
Santino-lab <80707158+Santino-lab@users.noreply.github.com>
2021-05-10 19:01:48 +0200

    .

---
+++ schleifenmm.py
@@ -0,0 +1,22 @@
+
+
+höhe =10
+for i in range (5):
+    
+    string= " " * (4-i) + "*" * (1+2 * i)
+    print (string)
+
+
+höhe =10   
+for i in range (5):
+    string= " " * (i) + "*" * (9 * i)
+    print (string)
+
+
+                                  
+
+#*****
+#*****
+#*****
+#*****
+#*****
