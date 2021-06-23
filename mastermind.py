59af8be159fdce9a8ad4e17cabd142d733060a72
Santino-lab <80707158+Santino-lab@users.noreply.github.com>
2021-06-23 18:48:54 +0200

    Update mastermind.py

--- mastermind.py
+++ mastermind.py
@@ -5,9 +5,22 @@ import random
 farben = ["blau", "grün", "gelb", "rot", "lila" ]
 
 ##wir machen die kombination ###
-kombination= []
-kombination.append(random.choice(farben))
-kombination.append(random.choice(farben))
-kombination.append(random.choice(farben))
+kombination = []
+farbe1 = random.choice (farben)
+kombination.append(farbe1)
+farben.remove (farbe1)
+farbe2 = random.choice (farben)
+kombination.append(farbe2)
+farben.remove (farbe2)
+farbe3 = random.choice (farben)
+kombination.append(farbe3)
+farben.remove (farbe3)
+farbe4 = random.choice (farben)
+kombination.append(farbe4)
+farben.remove (farbe4)
+farbe5 = random.choice (farben)
+kombination.append(farbe5)
+farben.remove (farbe5)
 
-print (kombination)
+
+print (kombination) 
