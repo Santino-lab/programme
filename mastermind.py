2473b71cd7bc1d0365ee038c509fa768cebea7ab
Santino-lab <80707158+Santino-lab@users.noreply.github.com>
2021-06-30 17:59:15 +0200

    Update mastermind.py

--- mastermind.py
+++ mastermind.py
@@ -7,25 +7,25 @@ farben = ["blau", "grün", "gelb", "rot", "lila" ]
 ##wir machen die kombination ###
 kombination = []
 farbe1 = random.choice (farben)
-kombination.append(farbe1)
+kombination.append (farbe1)
 farben.remove (farbe1)
 farbe2 = random.choice (farben)
-kombination.append(farbe2)
+kombination.append (farbe2)
 farben.remove (farbe2)
 farbe3 = random.choice (farben)
-kombination.append(farbe3)
+kombination.append (farbe3)
 farben.remove (farbe3)
 farbe4 = random.choice (farben)
-kombination.append(farbe4)
+kombination.append (farbe4)
 farben.remove (farbe4)
 farbe5 = random.choice (farben)
-kombination.append(farbe5) 
+kombination.append (farbe5) 
 farben.remove (farbe5)
 
-### wir fragen den spieler nach seiner wahl ###
+###wir fragen den spieler nach seiner wahl ###
 benutzerwahl = input ("welche kombination von farben willst du wählen ? ")
 benutzerwahl_liste = benutzerwahl.split()
-print (benutzerwahl_liste)
+print (benutzerwahl_liste)                         
 
+ 
 
-##was macht split ? 
