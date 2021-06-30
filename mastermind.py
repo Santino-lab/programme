ab4718e728fc6977c0c8042b8b88beda63a293d2
Santino-lab <80707158+Santino-lab@users.noreply.github.com>
2021-06-30 18:42:10 +0200

    Update mastermind.py

--- mastermind.py
+++ mastermind.py
@@ -22,35 +22,40 @@ farbe5 = random.choice (farben)
 kombination.append (farbe5) 
 farben.remove (farbe5)"""
 
+schwarz = -1
+while schwarz <3:    
+
+
+
 ###wir fragen den spieler nach seiner wahl ###
-benutzerwahl = input ("welche kombination von farben willst du wählen ? ")
-benutzerwahl_liste = benutzerwahl.split()
-print (benutzerwahl_liste)                     
+    benutzerwahl = input ("welche kombination von farben willst du wählen ? ")
+    benutzerwahl_liste = benutzerwahl.split()
+    print (benutzerwahl_liste)                     
 
-#### was macht split ? sie trennen einen string und stecken ihn in eine liste ###
+    #### was macht split ? sie trennen einen string und stecken ihn in eine liste ###
 
 
 
-schwarz = 0
-weiß = 0
-if kombination[0] == benutzerwahl_liste [0]:
-    schwarz += 1
-if kombination[1] == benutzerwahl_liste [0]:
-    weiß += 1
-if kombination[2] == benutzerwahl_liste [0]:
-    schwarz += 1
-if kombination[0] == benutzerwahl_liste [1]:
-    weiß += 1
-if kombination[1] == benutzerwahl_liste [1]:
-    schwarz += 1
-if kombination[2] == benutzerwahl_liste [1]:
-    weiß += 1
-if kombination[0] == benutzerwahl_liste [2]:
-    weiß += 1
-if kombination[1] == benutzerwahl_liste [2]:
-    weiß += 1
-if kombination[2] == benutzerwahl_liste [2]:
-    schwarz += 1
+    schwarz = 0
+    weiß = 0
+    if kombination[0] == benutzerwahl_liste [0]:
+        schwarz += 1
+    if kombination[1] == benutzerwahl_liste [0]:
+        weiß += 1
+    if kombination[2] == benutzerwahl_liste [0]:
+        schwarz += 1
+    if kombination[0] == benutzerwahl_liste [1]:
+        weiß += 1
+    if kombination[1] == benutzerwahl_liste [1]:
+        schwarz += 1
+    if kombination[2] == benutzerwahl_liste [1]:
+        weiß += 1
+    if kombination[0] == benutzerwahl_liste [2]:
+        weiß += 1
+    if kombination[1] == benutzerwahl_liste [2]:
+        weiß += 1
+    if kombination[2] == benutzerwahl_liste [2]:
+        schwarz += 1
 
-print ("schwarz:"schwarz)
-print ("weiß"weiß)
+    print ("schwarz:",schwarz)
+    print ("weiß",weiß)
