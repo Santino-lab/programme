e7e1a217a78b5d985a88942aa4d863229f46cd99
Santino-lab <80707158+Santino-lab@users.noreply.github.com>
2021-07-14 18:28:00 +0200

    rekursion

---
+++ rekursion.py
@@ -0,0 +1,96 @@
+###eine rekursive funktion ist eine funktion die sich selbst aufruft###
+
+"""
+def fibonacci(n):
+    if n == 0:
+     return 0 
+    if n == 1:
+     return 1
+    nminus1tefibonaccizahl = fibonacci (n - 1)
+    nminus2tefibonaccizahl = fibonacci (n - 2)
+    ntefibonaccizahl = nminus1tefibonaccizahl + nminus2tefibonaccizahl
+    return ntefibonaccizahl
+
+
+print(fibonacci(333))
+"""
+
+
+
+
+
+
+
+
+
+
+
+def erstes (liste):
+    retern (liste) [0]
+
+erstes ([4,2,8])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
