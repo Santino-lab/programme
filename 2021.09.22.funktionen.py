2b98e86e6e63da3d1c54e44e19541640d8c79ea4
Santino-lab <80707158+Santino-lab@users.noreply.github.com>
2021-09-29 17:35:38 +0200

    12

---
+++ 2021.09.22.funktionen.py
@@ -0,0 +1,98 @@
+"""def f1():
+    print ("hallo mein name ist f1 ")
+
+
+f1()
+
+
+def f2 (arg=0):
+    print (arg)
+f2("22")
+f2()
+
+def f3 (arg1,arg2):
+    if arg1 < arg2:
+        print (arg2)
+    if arg2 < arg1:
+        print (arg1)
+
+
+f3(2,9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def f4 (arg1,arg2,arg3):
+    if arg1 < arg2 < arg3:
+        print (arg3)
+    if arg2 < arg3 < arg1:
+        print (arg1)
+    if arg3 < arg1 < arg2:
+        print (arg1)
+
+f4(12,2,3)
+
+def bis(arg):
+    laufvariable  = 0 
+    while laufvariable <= arg:
+        print (laufvariable)
+        laufvariable = laufvariable +1
+
+#bis(100)
+
+
+def gerade(zahl):
+    laufvariable  = 0 
+    while laufvariable <= zahl:
+        zähler = 1 - zähler
+        laufvariable = laufvariable +1
+"""
+
+def summebis(zahl):
+    
+    zähler = 0
+    
+    summe = 0
+    
+    while zähler <= zahl:
+        summe = summe + zähler
+        
+        zähler = zähler +1 
+        
+    print (summe)
+
+summebis (4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
