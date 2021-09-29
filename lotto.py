9182074c132ff2baf3914fdf2e9046c91d4280d5
Santino-lab <santino@siguron.de>
2021-09-29 20:58:48 +0200

    r

--- lotto.py
+++ lotto.py
@@ -1,6 +1,8 @@
 import random
 
 for i in range (10):
+    benutzerwahl = input ("welche zahl willst du wählen")
+    
     liste = random.randint (1,10)
 print (liste)
 
--- sortieren.py
+++ sortieren.py
@@ -1,4 +1,28 @@
+"""sortiert = []
 liste = [2134,3421,465,84,67896789,97,96574,99]
-o = i
-for i in liste [2134,3421,465,84,67896789,97,96574,99] : 
-    if  
+o = 5464333333444446
+for i in liste : 
+    if i < o :
+    
+    liste . append
+    sortiert . remove 
+    print (o)"""
+
+    ### definire eine liste von zahlen und speichere diese in einer varieable 
+
+asd = []
+
+liste = [2134,3421,465,84,67896789,97,96574,99]
+
+summe = 0 
+
+for i in range (asd*liste) : 
+    
+    (summe) + 1 
+    
+    print (asd)
+
+    ###  berechne das produkt der aufgelisteten zahlen .
+
+
+
